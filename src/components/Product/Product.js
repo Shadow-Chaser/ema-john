2039fa5bf55ed1a0cf93f81d25b7f9fe19ad7700
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-02-26 18:50:44 +0600

    font awesome added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
@@ -13,10 +15,9 @@ const Product = (props) => {
                 <h4 className='product-name'>{props.product.name}</h4>
                 <br/>
                 <p><small>by : {props.product.seller}</small></p>
-
                 <p>${props.product.price}</p>
                 <p><small>Only {props.product.stock} left in stock - Order Soon</small></p>
-                <br/>
+                <button className='main-button'> <FontAwesomeIcon icon={faCoffee} /> Add to Cart</button>
 
             </div>
            
