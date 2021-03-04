b7e53ff13efb874b8f46b9c5118a3d4febe5d05a
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-03-04 20:25:10 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Review from './components/Review/Review';
 
 function App() {
   return (
     <div>
+
       <Header></Header>
-      <Shop></Shop>
+      {/* <Shop></Shop> */}
+
+      <Router>
+      <Switch>
+        <Router path="/shop">
+          <Shop></Shop>
+
+        </Router>
+
+        <Router path="/review">
+          <Review></Review>
+        </Router>
+
+      </Switch>
+
+      </Router>
+
     </div>
   );
 }
